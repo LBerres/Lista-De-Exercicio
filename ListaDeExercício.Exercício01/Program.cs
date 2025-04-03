d5e69e8db72b1bd179364f5d6dba8d8623904a82
LBerres <Leandro.motorhead@gmail.com>
2025-04-03 19:45:43 -0300

    Add project files.

---
+++ ListaDeExercício.Exercício01/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ListaDeExercício.Exercício01
+{
+    internal class Program
+    {
+        // Crie um Programa Para Calcular o Volume de Uma Caixa Retangular
+        // VOLUME_DA_CAIXA = COMPRIMENTO * LARGURA * ALTURA
+        static void Main(string[] args)
+        {
+            #region Input de Dados
+            Console.WriteLine("Digire o Comprimento da Caixa: ");
+            decimal comprimento = Convert.ToDecimal(Console.ReadLine());
+            
+
+            Console.WriteLine("Digire a Largura da Caixa: ");
+            decimal largura = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Digire a Altura da Caixa: ");
+            decimal altura = Convert.ToDecimal(Console.ReadLine());
+
+            #endregion
+
+            #region Processamento
+
+            decimal resultado = comprimento * largura * altura;
+
+            #endregion
+
+            #region OutPut
+
+            Console.WriteLine($"O Volume da Caixa é de {resultado}.");
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ListaDeExercícios.Exercício02/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ListaDeExercícios.Exercício02
+{
+    internal class Program
+    {
+
+        // Crie um Programa Para Calcular o Volume de Um Cilindro
+        // V = PI * R * R * A
+        static void Main(string[] args)
+        {
+            // Imput de Dados
+            Console.Write("Digite o Raio do Cilindro: ");
+            double raio = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a Altura do Cilindro: ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            // Processamento
+            double volume = Math.PI * Math.Pow(raio, 2) * altura;
+
+            // Output
+
+        }
+    }
+}
